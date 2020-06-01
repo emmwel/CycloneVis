7e92e33894a3d1a1742148140a3883d53573e771
Emma <emma.nilsson96@gmail.com>
2020-06-01 11:08:05 +0200

    made it possible to use shaders, created a textured mesh renderer

--- include/inviwo/cyclonevis/processors/texturedmeshrenderer.h
+++ include/inviwo/cyclonevis/processors/texturedmeshrenderer.h
@@ -30,9 +30,22 @@
 #pragma once
 
 #include <inviwo/cyclonevis/cyclonevismoduledefine.h>
+#include <inviwo/core/common/inviwo.h>
 #include <inviwo/core/processors/processor.h>
-#include <inviwo/core/properties/ordinalproperty.h>
+#include <inviwo/core/interaction/cameratrackball.h>
+#include <inviwo/core/ports/meshport.h>
 #include <inviwo/core/ports/imageport.h>
+#include <inviwo/core/properties/boolproperty.h>
+#include <inviwo/core/properties/buttonproperty.h>
+#include <inviwo/core/properties/cameraproperty.h>
+#include <inviwo/core/properties/ordinalproperty.h>
+#include <inviwo/core/properties/compositeproperty.h>
+#include <inviwo/core/properties/simplelightingproperty.h>
+#include <inviwo/core/rendering/meshdrawer.h>
+
+#include <modules/opengl/shader/shader.h>
+
+#include <vector>
 
 namespace inviwo {
 
@@ -53,16 +66,43 @@ namespace inviwo {
 class IVW_MODULE_CYCLONEVIS_API TexturedMeshRenderer : public Processor {
 public:
     TexturedMeshRenderer();
+
+	// delete copy and assigment
+	TexturedMeshRenderer(const TexturedMeshRenderer&) = delete;
+	TexturedMeshRenderer& operator=(const TexturedMeshRenderer&) = delete;
+
     virtual ~TexturedMeshRenderer() = default;
 
+	virtual void initializeResources() override;
     virtual void process() override;
 
     virtual const ProcessorInfo getProcessorInfo() const override;
     static const ProcessorInfo processorInfo_;
 
 private:
-    ImageOutport outport_;
-    FloatVec3Property position_;
+    MeshInport inport_;
+	ImageInport textureInport_;
+	ImageInport backgroundInport_;
+	ImageOutport outport_;
+
+	CameraProperty camera_;
+	CameraTrackball trackball_;
+
+	BoolProperty overrideColorBuffer_;
+	FloatVec4Property overrideColor_;
+
+	CompositeProperty meshProperties_;
+	OptionPropertyInt cullFace_;
+	BoolProperty enableDepthTest_;
+	SimpleLightingProperty lightingProperty_;
+
+	CompositeProperty layers_;
+	BoolProperty colorLayer_;
+	BoolProperty texCoordLayer_;
+	BoolProperty normalsLayer_;
+	BoolProperty viewNormalsLayer_;
+
+	Shader shader_;
 };
 
 }  // namespace inviwo
--- src/processors/texturedmeshrenderer.cpp
+++ src/processors/texturedmeshrenderer.cpp
@@ -28,6 +28,25 @@
  *********************************************************************************/
 
 #include <inviwo/cyclonevis/processors/texturedmeshrenderer.h>
+#include <modules/opengl/geometry/meshgl.h>
+#include <inviwo/core/datastructures/buffer/bufferramprecision.h>
+#include <inviwo/core/interaction/trackball.h>
+#include <inviwo/core/common/inviwoapplication.h>
+#include <inviwo/core/rendering/meshdrawerfactory.h>
+#include <modules/base/algorithm/mesh/axisalignedboundingbox.h>
+#include <modules/base/algorithm/mesh/meshcameraalgorithms.h>
+#include <modules/opengl/rendering/meshdrawergl.h>
+#include <inviwo/core/processors/processor.h>
+#include <modules/opengl/shader/shader.h>
+#include <modules/opengl/texture/textureutils.h>
+#include <modules/opengl/shader/shaderutils.h>
+#include <modules/opengl/texture/textureunit.h>
+#include <modules/opengl/openglutils.h>
+
+#include <modules/base/algorithm/dataminmax.h>
+#include <inviwo/core/algorithm/boundingbox.h>
+
+#include <limits>
 
 namespace inviwo {
 
@@ -35,23 +54,128 @@ namespace inviwo {
 const ProcessorInfo TexturedMeshRenderer::processorInfo_{
     "org.inviwo.TexturedMeshRenderer",      // Class identifier
     "Textured Mesh Renderer",                // Display name
-    "Undefined",              // Category
+    "CycloneVis",              // Category
     CodeState::Experimental,  // Code state
-    Tags::None,               // Tags
+    Tags::GL,               // Tags
 };
 const ProcessorInfo TexturedMeshRenderer::getProcessorInfo() const { return processorInfo_; }
 
 TexturedMeshRenderer::TexturedMeshRenderer()
     : Processor()
-    , outport_("outport")
-    , position_("position", "Position", vec3(0.0f), vec3(-100.0f), vec3(100.0f)) {
+	, inport_("geometry")
+	, textureInport_("textureInport", true)
+	, backgroundInport_("backgroundInport", true)
+	, outport_("image")
+	, camera_("camera", "Camera", util::boundingBox(inport_))
+	, trackball_(&camera_)
+	, overrideColorBuffer_("overrideColorBuffer", "Override Color Buffer", false,
+		InvalidationLevel::InvalidResources)
+	, overrideColor_("overrideColor", "Override Color", vec4(0.75f, 0.75f, 0.75f, 1.0f), vec4(0.0f),
+		vec4(1.0f))
+	, meshProperties_("geometry", "Geometry Rendering Properties")
+	, cullFace_("cullFace", "Cull Face",
+		{ {"culldisable", "Disable", GL_NONE},
+		 {"cullfront", "Front", GL_FRONT},
+		 {"cullback", "Back", GL_BACK},
+		 {"cullfrontback", "Front & Back", GL_FRONT_AND_BACK} },
+		0)
+	, enableDepthTest_("enableDepthTest_", "Enable Depth Test", true)
+	, lightingProperty_("lighting", "Lighting", &camera_)
+	, layers_("layers", "Output Layers")
+	, colorLayer_("colorLayer", "Color", true, InvalidationLevel::InvalidResources)
+	, texCoordLayer_("texCoordLayer", "Texture Coordinates", false,
+		InvalidationLevel::InvalidResources)
+	, normalsLayer_("normalsLayer", "Normals (World Space)", false,
+		InvalidationLevel::InvalidResources)
+	, viewNormalsLayer_("viewNormalsLayer", "Normals (View space)", false,
+		InvalidationLevel::InvalidResources)
+	, shader_("texturedmeshrendering.vert", "texturedmeshrendering.frag", false) {
 
-    addPort(outport_);
-    addProperty(position_);
+	addPort(inport_);
+	addPort(textureInport_);
+	addPort(backgroundInport_).setOptional(true);
+	addPort(outport_);
+
+	addProperties(camera_, meshProperties_, lightingProperty_, trackball_, layers_);
+
+	meshProperties_.addProperties(cullFace_, enableDepthTest_, overrideColorBuffer_,
+		overrideColor_);
+
+	overrideColor_.setSemantics(PropertySemantics::Color)
+		.visibilityDependsOn(overrideColorBuffer_, [](const BoolProperty p) { return p.get(); });
+
+	layers_.addProperties(colorLayer_, texCoordLayer_, normalsLayer_, viewNormalsLayer_);
+
+	shader_.onReload([this]() { invalidate(InvalidationLevel::InvalidResources); });
+}
+
+//TexturedMeshRenderer::~TexturedMeshRenderer() = default;
+
+void TexturedMeshRenderer::initializeResources() {
+	utilgl::addShaderDefines(shader_, lightingProperty_);
+
+	auto vert = shader_.getVertexShaderObject();
+	auto frag = shader_.getFragmentShaderObject();
+
+	vert->setShaderDefine("OVERRIDE_COLOR_BUFFER", overrideColorBuffer_);
+	frag->setShaderDefine("COLOR_LAYER", colorLayer_);
+
+	// first two layers (color and picking) are reserved
+	int layerID = 2;
+	frag->setShaderDefine("TEXCOORD_LAYER", texCoordLayer_);
+	if (texCoordLayer_) frag->addOutDeclaration("tex_coord_out", layerID++);
+
+	frag->setShaderDefine("NORMALS_LAYER", normalsLayer_);
+	if (normalsLayer_) frag->addOutDeclaration("normals_out", layerID++);
+
+	frag->setShaderDefine("VIEW_NORMALS_LAYER", viewNormalsLayer_);
+	if (viewNormalsLayer_) frag->addOutDeclaration("view_normals_out", layerID++);
+
+	// get a hold of the current output data
+	auto prevData = outport_.getData();
+	auto numLayers = static_cast<std::size_t>(layerID - 1);  // Don't count picking
+	if (prevData->getNumberOfColorLayers() != numLayers) {
+		// create new image with matching number of layers
+		auto image = std::make_shared<Image>(prevData->getDimensions(), prevData->getDataFormat());
+		// update number of layers
+		for (auto i = image->getNumberOfColorLayers(); i < numLayers; ++i) {
+			image->addColorLayer(std::shared_ptr<Layer>(image->getColorLayer(0)->clone()));
+		}
+		outport_.setData(image);
+	}
+
+	shader_.build();
 }
 
 void TexturedMeshRenderer::process() {
-    // outport_.setData(myImage);
+	utilgl::activateTargetAndClearOrCopySource(outport_, backgroundInport_);
+	shader_.activate();
+
+	// Textures
+	TextureUnit colorTexUnit;
+
+	if (textureInport_.isReady()) {
+		utilgl::bindColorTexture(textureInport_, colorTexUnit.getEnum());
+	}
+
+	shader_.setUniform("inportTexture", colorTexUnit.getUnitNumber());
+
+	utilgl::GlBoolState depthTest(GL_DEPTH_TEST, enableDepthTest_);
+	utilgl::CullFaceState culling(cullFace_);
+	utilgl::BlendModeState blendModeStateGL(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	utilgl::setUniforms(shader_, camera_, lightingProperty_, overrideColor_);
+	for (auto mesh : inport_) {
+		utilgl::setShaderUniforms(shader_, *mesh, "geometry");
+		shader_.setUniform("pickingEnabled", meshutil::hasPickIDBuffer(mesh.get()));
+		MeshDrawerGL::DrawObject drawer{ mesh->getRepresentation<MeshGL>(),
+										mesh->getDefaultMeshInfo() };
+		drawer.draw();
+	}
+
+	shader_.deactivate();
+	utilgl::deactivateCurrentTarget();
+	TextureUnit::setZeroUnit();
 }
 
 }  // namespace inviwo
